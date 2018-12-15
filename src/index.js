661017cdd94c8666c47c42ef14da8a951312ab01
Thundermaker300 <thundermaker300@gmail.com>
2018-12-15 02:38:16 -0500

    Added client thing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 var config = require('./config/config.json')
 var private = require('./config/private.json')
+var Discord = require('discord.js')
+var bot = new Discord.Client()
+
+bot.login(private.token)
